b7dc8f40124f40dbbc2a25b1e8fe95f8c32fd491
David Dean <vermicious.knid.d@gmail.com>
2018-09-16 21:03:54 -0700

    Add nested loop to get products from each brand. Update dogFood response object.

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+/*
+  * Scraping: https://dev.to/aurelkurtula/introduction-to-web-scraping-with-nodejs-9h2
+  * Axios: https://laracasts.com/discuss/channels/vue/for-loop-and-axios and https://github.com/axios/axios
+*/
 const express = require('express')
 const app = express()
 const port = 3000
@@ -7,24 +11,51 @@ const fs = require('fs')
 
 // API: 5 star canned dog food
 app.get('/five-star-cans', function (req, res) {
+  let dogFoodDB = []
   axios.get('https://www.dogfoodadvisor.com/dog-food-reviews/wet/5-star/')
     .then((response) => {
-      if(response.status === 200) {
-      const html = response.data
-        const $ = cheerio.load(html)
-        const title = $('.entry-title').text()
+      if (response.status === 200) {
+        const htmlAll = response.data
+        const $ = cheerio.load(htmlAll)
+        const titleAll = $('.entry-title').text()
         let dogFoodList = []
         $('.listing-shortcode-output > li').each(function(i, elem) {
           dogFoodList[i] = {
-            title: $(this).find('a').text().trim(),
+            name: $(this).find('a').text().trim(),
             url: $(this).find('a').attr('href')
           }
         })
-        res.send({ title: title, dogFoodList: dogFoodList })
+        return dogFoodList
       }
-    },
-    (error) => console.log(err)
-  )
+    }, (error) => console.log(error) )
+    .then((response) => {
+      let promises = []
+
+      response.forEach(function(brand){
+        promises.push(axios.get(brand.url))
+      })
+
+      axios.all(promises)
+        .then((results) => {
+          results.forEach((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const title = $('.entry-title').text()
+            // let dogFoodBrands = []
+            $('div.entry-content > ul > li').each(function(i, elem) {
+              dogFoodDB.push({
+                brandName: title,
+                productName: $(this).text().trim(),
+                url: response.config.url
+              })
+            })
+          })
+          console.log(dogFoodDB)
+          res.send(dogFoodDB)
+        })
+
+    }, (error) => console.log(error) )
+
 })
 
 // Express server
